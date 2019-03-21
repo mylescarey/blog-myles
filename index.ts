24f77ff99f3f4f34a7287989294a5f08f55559df
Myles Carey <myles.carey@Nology.local>
2019-03-21 16:35:58 +0000

    Committing with latest design changes

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 let blogPosts: BlogPost[] = [];
 
+let archive = [];
+
 const createBlogPost = () => {
     let form = document.getElementById("blog-post-form");
     let title = form["blog-title"].value;
@@ -7,8 +9,9 @@ const createBlogPost = () => {
 
     blogPosts.push(new BlogPost(title, post));
     // console.log(blogPosts);
-    console.log(title);
     outputBlog(title);
+    outputBlogBody(post);
+    // moveOldPostsToArchive();
 }
 
 class BlogPost {
@@ -23,19 +26,49 @@ class BlogPost {
 
 const outputBlog = (title) => {
     let target = document.getElementById('output-blog');
-    let node = document.createElement("div"); 
+    let node = document.createElement("h3"); 
     let textnodetitle = document.createTextNode(title);
     node.appendChild(textnodetitle)
     // let textnodepost = document.createTextNode("blog-post");
     target.appendChild(node);
 }
 
+const outputBlogBody = (post) => {
+    let target = document.getElementById('output-blog');
+    let node = document.createElement("p"); 
+    let textnodepost = document.createTextNode(post);
+    node.appendChild(textnodepost)
+    // let textnodepost = document.createTextNode("blog-post");
+    target.appendChild(node);
+}
 
 
+// Archive - Needs Work
+/* const outputBlogArchive = (title) => {
+    let target = document.getElementById('archive');
+    let node = document.createElement("h3"); 
+    let textnodetitle = document.createTextNode(title);
+    node.appendChild(textnodetitle)
+    // let textnodepost = document.createTextNode("blog-post");
+    target.appendChild(node);
+}
 
+const outputBlogBodyArchive = (post) => {
+    let target = document.getElementById('archive');
+    let node = document.createElement("p"); 
+    let textnodepost = document.createTextNode(post);
+    node.appendChild(textnodepost)
+    // let textnodepost = document.createTextNode("blog-post");
+    target.appendChild(node);
+}
 
+const moveOldPostsToArchive = () => {
+    let oldPost = blogPosts[0]
+    archive.push(oldPost);
+    blogPosts.shift;
+}
 
-
+*/
 
 
 // Liams code - Not clean but functional
