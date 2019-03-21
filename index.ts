c83dbc02029b7ace7cc7c603267be06f3e3129ef
Myles Carey <myles.carey@Nology.local>
2019-03-21 12:09:39 +0000

    Outputs the title

--- index.ts
+++ index.ts
@@ -1,7 +1,59 @@
-const sayHello = () => {
-    alert('hello');
+let blogPosts: BlogPost[] = [];
+
+const createBlogPost = () => {
+    let form = document.getElementById("blog-post-form");
+    let title = form["blog-title"].value;
+    let post = form["blog-post"].value;
+
+    blogPosts.push(new BlogPost(title, post));
+    // console.log(blogPosts);
+    console.log(title);
+    outputBlog(title);
 }
 
-const submitForm = () => {
-    
+class BlogPost {
+    blogTitle: string;
+    blogPost: string;
+
+    constructor(title: string, post: string) {
+        this.blogTitle = title;
+        this.blogPost = post;
+    }
 }
+
+const outputBlog = (title) => {
+    let target = document.getElementById('output-blog');
+    let node = document.createElement("div"); 
+    let textnodetitle = document.createTextNode(title);
+    node.appendChild(textnodetitle)
+    // let textnodepost = document.createTextNode("blog-post");
+    target.appendChild(node);
+}
+
+
+
+
+
+
+
+
+
+// Liams code - Not clean but functional
+/* const createBlogPost = () => {
+    let form = document.getElementById("blog-post-form");
+    let title = form["blog-title"].value;
+    let post = form["blog-post"].value;
+    blogPosts.push(new BlogPost(title, post));
+    populateBlogPosts();
+}
+
+const printBlogPost = (blogPost: BlogPost) => {
+    document.getElementById("blog-posts").innerHTML += "<p>" + blogPost.title + "</p>";
+}
+
+const populateBlogPosts = () => {
+    document.getElementById("blog-posts").innerHTML = ""
+    blogPosts.forEach(printBlogPost);
+}
+
+*/
